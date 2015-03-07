478659e98777e1020d6dc3b031c772f96773a82d
anthony <anthony.hivert@gmail.com>
2015-03-07 11:17:10 +0100

    Stream when cache is ok

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
+var CACHE_PERCENT = 3;
 
 var torrentDir = 'torrent-stream';
 var torrentStream = require('torrent-stream');
 var path = require('path');
 
-var engine = torrentStream('magnet:?xt=urn:btih:BB43CF1DC5B200BA37679DB96375A8190D933C2E&dn=Big+Hero+6+%282014%29+%5B720p%5D&tr=http%3A%2F%2Ftracker.yify-torrents.com%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.org%3A80&tr=udp%3A%2F%2Ftrackr.sytes.net%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337');
+
+
+var engine = torrentStream('magnet:?xt=urn:btih:130C1E9D107B96D6E63DC7FE5EBF1EE59AF044E1&dn=Elysium+%282013%29+%5B1080p%5D&tr=http%3A%2F%2Ftracker.yify-torrents.com%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.org%3A80&tr=udp%3A%2F%2Ftrackr.sytes.net%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fopen.demonii.com%3A1337');
 
 engine.on('ready', function() {
     var fileToPlay = null;
     console.log('engine:', engine);
     engine.files.forEach(function(file) {
         console.log('filename:', file.name);
-        // get the biggest file and test if the format is playable
-        if( (fileToPlay === null || fileToPlay.length < file.length)){
-            fileToPlay = file;
+        // get the biggest file
+        if ((fileToPlay === null || fileToPlay.length < file.length)) {
+            fileToPlay = file
         }
-
     });
     console.log('filetoPlay:', path.join(this.path, fileToPlay.path));
 
-    var video = document.getElementById('video');
-    video.setAttribute("src", path.join(this.path, fileToPlay.path) );
-
-
+    var video = videojs('video');
     var stream = fileToPlay.createReadStream();
 
-    video.load();
-    video.play();
+    var cache = 0;
+    // check size downloaded then play
+    cache = setInterval(function() {
+        var percent = engine.swarm.downloaded * 100 / stream.length;
+        console.log('percent:', percent);
+        if (percent >= CACHE_PERCENT) {
+            video.src(path.join(engine.path, fileToPlay.path));
+            video.play();
+            clearInterval(cache);
+
+        }
+
+    }, 3000);
+
+
 });
